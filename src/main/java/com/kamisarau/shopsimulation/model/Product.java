3da2b711ca3a947a7c9512ff9a98ecf48a403828
Anton_Kamisarau <komiktony@gmail.com>
2019-11-04 16:45:43 +0300

    Table inheritance problem solved

--- src/main/java/com/kamisarau/shopsimulation/model/Product.java
+++ src/main/java/com/kamisarau/shopsimulation/model/Product.java
@@ -1,20 +1,66 @@
 package com.kamisarau.shopsimulation.model;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
 @Data
-public class Product extends RectangularShape implements Serializable {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+@Accessors(chain = true)
+@DiscriminatorValue("product")
+@NoArgsConstructor
+public class Product extends AbstractRectangularShape implements Serializable, Storable {
+
     @ManyToOne(fetch = FetchType.LAZY)
     private Category category;
     private String name;
     private double price;
-    @ManyToOne
-    private Shelf shelf;
+
+    public Product(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public int getHeight() {
+        return height;
+    }
+
+    @Override
+    public int getWidth() {
+        return width;
+    }
+
+    @Override
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    @Override
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    @Override
+    public int getX() {
+        return x;
+    }
+
+    @Override
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    @Override
+    public void setY(int y) {
+        this.y = y;
+    }
 }
