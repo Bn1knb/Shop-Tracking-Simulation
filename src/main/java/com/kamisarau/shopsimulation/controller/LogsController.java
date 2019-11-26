89b0565c38f012cab744b8831dcac75f3e79a412
Anton_Kamisarau <komiktony@gmail.com>
2019-11-26 18:55:20 +0300

    logs rest point added

---
+++ src/main/java/com/kamisarau/shopsimulation/controller/LogsController.java
@@ -0,0 +1,28 @@
+package com.kamisarau.shopsimulation.controller;
+
+import com.kamisarau.shopsimulation.service.LogServiceStrategy;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+import static com.kamisarau.shopsimulation.util.LogStrategyChooser.choose;
+
+@RestController("/logs")
+public class LogsController {
+
+    @GetMapping(produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public ResponseEntity<Resource> getLogs(@RequestParam(defaultValue = "all", name = "type") String type,
+                                            @RequestParam(required = false) Date date) {
+        LogServiceStrategy logServiceStrategy = choose(type);
+        ByteArrayResource resource = new ByteArrayResource(logServiceStrategy.getLogs(date));
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .body(resource);
+    }
+}
