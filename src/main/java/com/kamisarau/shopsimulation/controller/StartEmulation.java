18502523864497bdace491b9c3e1dd37b9b8419f
Anton_Kamisarau <komiktony@gmail.com>
2019-11-27 11:55:13 +0300

    hotfix

--- src/main/java/com/kamisarau/shopsimulation/controller/StartEmulation.java
+++ src/main/java/com/kamisarau/shopsimulation/controller/StartEmulation.java
@@ -1,14 +1,21 @@
 package com.kamisarau.shopsimulation.controller;
 
+import com.kamisarau.shopsimulation.model.Shelf;
 import com.kamisarau.shopsimulation.service.Randomiser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
 
 @RestController
 @RequestMapping("/start")
 public class StartEmulation {
+    private static final Random RANDOM = new Random();
     private Randomiser randomiser;
 
     @Autowired
@@ -18,8 +25,11 @@ public class StartEmulation {
 
     @GetMapping
     public ResponseEntity start(@RequestParam(defaultValue = "10", required = false) int number) {
+        //todo thinks of storing shelf in cache and lock
+        Shelf shelf = new Shelf().setHeight(RANDOM.nextInt(100)).setWidth(RANDOM.nextInt(100));
+
         randomiser.populateStorageWithProducts();
-        randomiser.doRandomOperations(number);
+        randomiser.doRandomOperations(number, shelf);
         return new ResponseEntity(HttpStatus.OK);
     }
 }
